029cebc7cfd40d64b287995ae9a5d7e792d17d68
local-admin <local-admin@local-admindeMacBook-Pro.local>
2024-07-17 15:38:05 +0800

    feat: add photos

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Image } from "@nextui-org/react";
+import { Image, Divider } from "@nextui-org/react";
 import Link from 'next/link'
 import './page.css'
 import listJson from './list.json'
@@ -19,14 +19,15 @@ export default function App() {
         <div className="w-comb relative m-[10px] opacity-60 hover:opacity-100 cursor-pointer" style={style}>
           <Image
             alt="Card background"
-            className="object-cover rounded-xl"
+            className="object-cover rounded-xl h-[230px]"
             src={item.imgSrc}
             width={width}
             height={height}
           />
           <div className="absolute top-20 z-50">
-            <h4 className="font-bold text-large text-center">{item.title}</h4>
+            <h4 className="font-bold text-large text-center text-white">{item.title}</h4>
           </div>
+          <div className="w-comb-border"></div>
         </div>
       </Link>
     )
@@ -34,13 +35,16 @@ export default function App() {
     return r
   }, [])
   return (
-    <div className="h-full">
-      <div className="list" >
-        {
-          list.map(i => (
-            <div className="flex">{...i}</div>
-          ))
-        }
+    <div className="h-screen bg-[#282C33] flex justify-center">
+      <div>
+        <div><span className="text-[#C778DD]">#</span><span className="text-white font-bold">Blog</span></div>
+        <div className="list" >
+          {
+            list.map(i => (
+              <div className="flex">{...i}</div>
+            ))
+          }
+        </div>
       </div>
     </div>
   );
